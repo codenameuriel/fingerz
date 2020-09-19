95e1dbbd000dbd808f83e435d13bec7b5864b029
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-09-19 12:28:01 -0400

    change styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ButtonStyles from './Button.module.css';
 
 const Button = props => {
   return (
-    <button onClick={props.handleClick}>{props.text}</button>
+    <button className={ButtonStyles.Button} onClick={props.handleClick}>{props.text}</button>
   );
 };
 
