0284daa7d38b16b0f8df62ce5ad830e473f646ae
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-01 15:06:04 -0400

    add navigation items to NavBar component

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBarStyles from './NavBar.module.css';
+import NavItem from '../NavItem/NavItem';
 
 const NavBar = () => {
   return (
     <header className={NavBarStyles.NavBar}>
       <nav>
         <ul>
+          <NavItem name="Home" />
+          <NavItem name="Words" />
         </ul>
       </nav>
     </header>
