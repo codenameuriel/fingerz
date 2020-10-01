443fa146d72c1cd1e039b9813d902358eb0d556c
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-01 14:37:41 -0400

    create NavBar compponent

---
+++ src/components/Navigation/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBarStyles from './NavBar.module.css';
+
+const NavBar = () => {
+  return (
+    <header className={NavBarStyles.NavBar}>
+      <nav>
+        <ul>
+
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
