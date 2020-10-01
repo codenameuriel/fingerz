ff4e57c997f3092a558ec4e34e1e100f82868b60
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-01 14:39:32 -0400

    add NavBar component to Layout component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Keyboard from '../../containers/Keyboard/Keyboard';
 import WordPanel from '../../containers/WordPanel/WordPanel';
 import LayoutStyles from './Layout.module.css';
+import NavBar from '../../components/Navigation/NavBar/NavBar';
 
 const Layout = () => {
   return (
     <div className={LayoutStyles.Layout}>
+        <NavBar />
         <WordPanel />
         <Keyboard />
     </div>
