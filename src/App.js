a9497be726bf126d0bc0ea596da740831f4b1406
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-03 00:30:31 -0400

    add Route to App  component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import TypeTest from './components/TypeTest/TypeTest';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
-
     return (
       <Layout>
-        <TypeTest />
+        <Switch>
+          <Route exact path="/" component={TypeTest} />
+        </Switch>
       </Layout>
     )
   }
