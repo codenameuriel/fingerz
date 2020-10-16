63f040c6bc996ffbb9da1cfce22e2b11ff97b9e0
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-16 13:21:50 -0400

    install axios package

--- src/containers/WordsPage/WordsPage.js
+++ src/containers/WordsPage/WordsPage.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import WordsPageStyles from './WordsPage.module.css';
 
 class WordsPage extends Component {
+  componentDidMount() {
+   // some code
+  }
+
+  
+  
   render() {
     return (
       <div className={WordsPageStyles.WordsPage}>
         <h1>Select Words Pack</h1>
         <p>Select different collection of words to improve typing</p>
+
+        <div></div>
       </div>
     );
   }
