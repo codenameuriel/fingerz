bed6099ea0a31f5e7143f95be22df1f585c97727
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-11-11 22:13:47 -0500

    refactor Timer component

--- src/containers/Timer/Timer.js
+++ src/containers/Timer/Timer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TimerStyles from './Timer.module.css';
+import { IoIosRefresh } from 'react-icons/io';
 
 const Timer = props => {
   let timeDisplay;
@@ -11,16 +12,23 @@ const Timer = props => {
   } else {
     timeDisplay = ':' + props.time;
   }
-
+  
   return (
     <div className={TimerStyles.Timer}>
       <p style={props.time < 10 ? { color: 'red' } : null}>{timeDisplay}</p>
-      <button 
+      <IoIosRefresh className={TimerStyles.ResetIcon}
+        disable={!props.timerStarted} 
+        style={props.timerStarted ? null : { background: 'transparent', height: '75px', color: '#161629', cursor: 'not-allowed' }}
+        onClick={() => {
+          if (props.timerStarted) props.stopTimer(props.activeTimer)
+          }} 
+      />
+      {/* <button 
         disable={!props.timerStarted} 
         style={props.timerStarted ? null : { backgroundColor: '#161629', cursor: 'not-allowed' }}
         onClick={() => {
           if (props.timerStarted) props.stopTimer(props.activeTimer)
-          }}>Restart</button>
+          }}>Restart</button> */}
     </div>
   );
 };
